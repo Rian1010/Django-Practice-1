6f7a5accd0e66f2a44485bfaec94f1d64045a815
Rian1010 <rtegally1098@gmail.com>
2020-04-11 19:18:36 +0200

    Remove a comment in the settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
-# DATABASE = {'default': dj_database_url.parse('postgres://iopaqnzajwfigq:a1ef69b6d27732802f160a2cfbb976c150e9eb30656d67e298f9678b3264f492@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d82sr5tg6ugvsm')}
 if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
